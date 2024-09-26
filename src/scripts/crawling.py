69753651ca5ead95ac1f4a350cb70af133d26059
Gabriel Schurr <gabriel.elias2000@outlook.de>
2024-09-26 17:59:58 +0200

    Add config.yaml for crawling

--- src/scripts/crawling.py
+++ src/scripts/crawling.py
@@ -9,13 +9,14 @@ from bs4 import BeautifulSoup
 from PIL import Image
 from tqdm import tqdm
 import io
+import yaml
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 # Set up logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    filename="flyer_downloader.log",
+    filename="leaflet_downloader.log",
     filemode="a",
 )
 
@@ -26,27 +27,17 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console.setFormatter(formatter)
 logging.getLogger("").addHandler(console)
 
-# Because some shitty supermarkets have special names
-# name in button != name in image url
-unique_names = {
-    "aldi-sy-d": "aldi-sud",
-    "aldi-sy-d-wein": "aldi-sud",
-    "denns-biomarkt": "denn-s-biomarkt",
-}
 
-
-class FlyerDownloader:
-    """A class to download supermarket flyer images from prospekteangebote.de"""
-
-    def __init__(self, markets_file):
+class leafletDownloader:
+    def __init__(self, config_file):
         """
-        Initialize the FlyerDownloader.
+        Initialize the leafletDownloader.
 
-        :param markets_file: Path to the file containing market URLs
+        :param config_file: Path to the YAML configuration file
         """
-        self.markets_file = markets_file
-        self.flyer_url = r"https://www.prospektangebote.de{flyer_href}"
-        self.flyer_ids = defaultdict(list)
+        self.config = self.load_config(config_file)
+        self.leaflet_url = r"https://www.prospektangebote.de{leaflet_href}"
+        self.leaflet_ids = defaultdict(list)
         self.session = requests.Session()
         self.session.headers.update(
             {
@@ -68,30 +59,39 @@ class FlyerDownloader:
         return name
 
     @staticmethod
-    def mask_name(name):
+    def load_config(config_file):
+        """
+        Load configuration from YAML file.
+
+        :param config_file: Path to the YAML configuration file
+        :return: Loaded configuration as a dictionary
+        """
+        with open(config_file, "r") as f:
+            return yaml.safe_load(f)
+
+    @staticmethod
+    def mask_name(name, to_mask):
         """
         Mask specific names in the string.
 
         :param name: The name to mask
         :return: Masked name
         """
-        to_mask = ["hitbbq", "lidlconnect"]
         for mask in to_mask:
             name = name.replace(mask, "<MASK>")
         return name
 
     def load_markets(self):
         """
-        Load market URLs from the file.
+        Load market URLs from the configuration.
 
         :return: List of market URLs
         """
-        with open(self.markets_file, "r") as f:
-            return f.read().split("\n")
+        return self.config["markets"]
 
     def process_market(self, market):
         """
-        Process a single market URL to extract flyer information.
+        Process a single market URL to extract leaflet information.
 
         :param market: Market URL to process
         """
@@ -113,64 +113,70 @@ class FlyerDownloader:
                 if div is None:
                     continue
                 if self.parse_name(supermarket_name) in self.mask_name(
-                    self.parse_name(self.mask_name(div["data-flyer-name"]))
+                    self.parse_name(self.mask_name(div["data-flyer-name"], self.config["to_mask"])),
+                    self.config["to_mask"],
                 ):
-                    flyer_id = div["data-flyer-id"]
-                    flyer_href = div.find("a", {"class": "btn"})["href"]
-                    this_flyer_url = self.flyer_url.format(flyer_href=flyer_href)
+                    leaflet_id = div["data-flyer-id"]
+                    leaflet_href = div.find("a", {"class": "btn"})["href"]
+                    this_leaflet_url = self.leaflet_url.format(leaflet_href=leaflet_href)
 
                     hidden_supermarket_name = re.search(
                         r"https://www.prospektangebote.de/anzeigen/angebote/(.*)",
-                        this_flyer_url,
+                        this_leaflet_url,
                     ).group(1)
                     hidden_supermarket_name = hidden_supermarket_name.split("-prospekt-")[0]
 
-                    hidden_supermarket_name = unique_names.get(hidden_supermarket_name, hidden_supermarket_name)
+                    hidden_supermarket_name = self.config["unique_names"].get(
+                        hidden_supermarket_name, hidden_supermarket_name
+                    )
 
-                    flyer_response = self.session.get(this_flyer_url)
-                    flyer_response.raise_for_status()
+                    leaflet_response = self.session.get(this_leaflet_url)
+                    leaflet_response.raise_for_status()
 
-                    flyer_pages = re.search(r"let flyerPages = (.+);", flyer_response.text)
-                    if flyer_pages:
-                        flyer_pages = flyer_pages.group(1).replace("\/", "/")
-                        flyer_pages = eval(flyer_pages)
-                        num_pages = len(flyer_pages)
+                    leaflet_pages = re.search(r"let flyerPages = (.+);", leaflet_response.text)
+                    if leaflet_pages:
+                        leaflet_pages = leaflet_pages.group(1).replace("\/", "/")
+                        leaflet_pages = eval(leaflet_pages)
+                        num_pages = len(leaflet_pages)
 
-                        self.flyer_ids[supermarket_name].append(
+                        self.leaflet_ids[supermarket_name].append(
                             {
                                 "hidden_supermarket_name": hidden_supermarket_name,
-                                "flyer_id": flyer_id,
-                                "flyer_href": flyer_href,
-                                "flyer_url": this_flyer_url,
+                                "leaflet_id": leaflet_id,
+                                "leaflet_href": leaflet_href,
+                                "leaflet_url": this_leaflet_url,
                                 "num_pages": num_pages,
                             }
                         )
         except Exception as e:
             logging.error(f"Error processing market {market}: {str(e)}")
 
-    def download_flyer(self, flyer_info):
+    def download_leaflet(self, leaflet_info):
         """
-        Download flyer images for a single flyer.
+        Download leaflet images for a single leaflet.
 
-        :param flyer_info: Dictionary containing flyer information
+        :param leaflet_info: Dictionary containing leaflet information
         """
-        supermarket_name = flyer_info["hidden_supermarket_name"]
-        flyer_id = flyer_info["flyer_id"]
-        num_pages = flyer_info["num_pages"]
+        supermarket_name = leaflet_info["hidden_supermarket_name"]
+        leaflet_id = leaflet_info["leaflet_id"]
+        num_pages = leaflet_info["num_pages"]
 
-        save_dir = os.path.join("flyers", supermarket_name, flyer_id)
+        out_dir = self.config["output_dir"]
+
+        save_dir = os.path.join(out_dir, supermarket_name, leaflet_id)
         os.makedirs(save_dir, exist_ok=True)
 
-        image_name = r"{supermarket_name}_{flyer_id}_{page}.jpg"
+        image_name = r"{supermarket_name}_{leaflet_id}_{page}.jpg"
 
-        # Check if the flyer is already fully downloaded
-        if self.is_flyer_complete(save_dir, num_pages):
-            logging.info(f"Flyer {flyer_id} for {supermarket_name} is already complete. Skipping.")
+        # Check if the leaflet is already fully downloaded
+        if self.is_leaflet_complete(save_dir, num_pages):
+            logging.info(f"leaflet {leaflet_id} for {supermarket_name} is already complete. Skipping.")
             return
 
         for page in range(1, num_pages + 1):
-            image_url = rf"https://img.offers-cdn.net/assets/uploads/flyers/{flyer_id}/largeWebP/{supermarket_name}-{page}-1.webp"
-            _image_name = image_name.format(supermarket_name=supermarket_name, flyer_id=flyer_id, page=page)
+            image_url = rf"https://img.offers-cdn.net/assets/uploads/flyers/{leaflet_id}/largeWebP/{supermarket_name}-{page}-1.webp"
+            #              https://img.offers-cdn.net/assets/uploads/flyers/2429386/largeWebP/globus-1-1.webp
+            _image_name = image_name.format(supermarket_name=supermarket_name, leaflet_id=leaflet_id, page=page)
             save_path = os.path.join(save_dir, _image_name)
 
             if not os.path.exists(save_path):
@@ -183,7 +189,7 @@ class FlyerDownloader:
 
                     # Save as JPG
                     img = img.convert("RGB")  # Convert to RGB mode if it's not already
-                    img.save(save_path, "JPEG", quality=95)
+                    img.save(save_path, "JPEG", quality=100)
 
                     logging.info(f"Downloaded and converted {save_path}")
                 except Exception as e:
@@ -191,12 +197,12 @@ class FlyerDownloader:
             else:
                 logging.info(f"Image {save_path} already exists. Skipping.")
 
-    def is_flyer_complete(self, save_dir, num_pages):
+    def is_leaflet_complete(self, save_dir, num_pages):
         """
-        Check if all pages of a flyer have been downloaded.
+        Check if all pages of a leaflet have been downloaded.
 
-        :param save_dir: Directory where flyer images are saved
-        :param num_pages: Total number of pages in the flyer
+        :param save_dir: Directory where leaflet images are saved
+        :param num_pages: Total number of pages in the leaflet
         :return: True if all pages are downloaded, False otherwise
         """
         for page in range(1, num_pages + 1):
@@ -206,26 +212,26 @@ class FlyerDownloader:
 
     def run(self):
         """
-        Run the flyer download process.
+        Run the leaflet download process.
         """
         markets = self.load_markets()
 
         # Process markets concurrently
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.config["workers"]["process"]) as executor:
             list(tqdm(executor.map(self.process_market, markets), total=len(markets), desc="Processing markets"))
 
-        # Download flyers concurrently
-        flyers_to_download = [flyer for flyers in self.flyer_ids.values() for flyer in flyers]
-        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        # Download leaflets concurrently
+        leaflets_to_download = [leaflet for leaflets in self.leaflet_ids.values() for leaflet in leaflets]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.config["workers"]["download"]) as executor:
             list(
                 tqdm(
-                    executor.map(self.download_flyer, flyers_to_download),
-                    total=len(flyers_to_download),
-                    desc="Downloading flyers",
+                    executor.map(self.download_leaflet, leaflets_to_download),
+                    total=len(leaflets_to_download),
+                    desc="Downloading leaflets",
                 )
             )
 
 
 if __name__ == "__main__":
-    downloader = FlyerDownloader(os.path.join(cur_dir, "markets.txt"))
+    downloader = leafletDownloader(os.path.join(cur_dir, "config.yaml"))
     downloader.run()
