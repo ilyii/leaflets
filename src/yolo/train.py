01b3e954421e3ac91da2fc7800a61ac0bc3db9f7
Gabriel Schurr <gabriel.elias2000@outlook.de>
2025-02-19 01:25:52 +0100

    Add evaluation scripts for yolo-seg

--- src/yolo/train.py
+++ src/yolo/train.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import yaml
 import os
+import argparse
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,9 +11,16 @@ def load_config(config_path):
         config = yaml.safe_load(f)
     return config
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train YOLO model")
+    parser.add_argument(
+        "--config", default=os.path.join(cur_dir, "train_configs.yaml"), type=str
+    )
+    args = parser.parse_args()
+    return args
 
 def train(cfg):
-    model = YOLO(cfg["model_path"])
+    model = YOLO(cfg["model_path"], verbose=False)
     results = model.train(
         data=cfg["data_path"],
         imgsz=cfg["imgsz"],
@@ -41,11 +49,12 @@ def validate(cfg, best_model_path):
 def main(cfg):
     results = train(cfg)
     run_path = results.save_dir
-    best_model_path = os.path.join(run_path, "weights", "best.pt")
-    val_results = validate(cfg, best_model_path)
-    print(val_results)
+    print(f"Results saved to {run_path}")
+    # best_model_path = os.path.join(run_path, "weights", "best.pt")
+    # val_results = validate(cfg, best_model_path)
 
 
 if __name__ == "__main__":
-    cfg = load_config(os.path.join(cur_dir, "train_configs.yaml"))
+    args = parse_args()
+    cfg = load_config(args.config)
     main(cfg)
---
+++ train_jobs.bat
@@ -0,0 +1,13 @@
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolo8s_train_configs.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolo8m_train_configs.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolo8l_train_configs.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolo11s_train_configs.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolo11m_train_configs.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolo11l_train_configs.yaml
+@REM
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolov8m_da/yolo8m_1.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolov8m_da/yolo8m_2.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolov8m_da/yolo8m_3.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolov8m_da/yolo8m_4.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolov8m_da/yolo8m_5.yaml
+@REM uv run src/yolo/train.py --config src/yolo/configs/yolov8m_da/yolo8m_6.yaml
